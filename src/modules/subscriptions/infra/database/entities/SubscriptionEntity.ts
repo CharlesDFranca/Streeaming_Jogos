a338fe3ea05a502a4b304f97de6c16b093522bfd
LeonardRuhmann <137243978+LeonardRuhmann@users.noreply.github.com>
2026-06-22 21:33:09 -0300

    feat: align domain entities and schema with logical model

--- src/modules/subscriptions/infra/database/entities/SubscriptionEntity.ts
+++ src/modules/subscriptions/infra/database/entities/SubscriptionEntity.ts
@@ -26,6 +26,9 @@ export class SubscriptionOrmEntity {
   @Column()
   status!: string;
 
+  @Column()
+  planId!: string;
+
   @ManyToOne(() => UserOrmEntity, (user) => user.subscriptions)
   user!: UserOrmEntity;
 
